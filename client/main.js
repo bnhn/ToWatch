b1b62e90da5e6085984a15752ed9f98470b34bc8
Rayy Benhin <bbenhin@gmail.com>
2016-06-16 07:52:10 +0000

    First commit

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+//import  {materialize-css} from 'node_modules/materialize-css';
+
+import './main.html';
+
+
